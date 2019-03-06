4aab6c18c28deef3240e99bcd2b1d3b9183e0d8a
mindip <oacidnim@g.ucla.edu>
2019-03-05 20:38:37 -0800

    article grid + aritcle card

--- src/components/ArticleGrid.tsx
+++ src/components/ArticleGrid.tsx
@@ -8,6 +8,17 @@ export class ArticleGrid extends React.Component {
       justify-content: space-around;
       flex-wrap: wrap;
     `
-    return <div className={gridStyle}>{this.props.children}</div>
+    const gridStyles1 = css`
+      display: grid;
+      grid-template-columns: repeat(3, 1fr);
+      grid-gap: 16px;
+      @media (max-width: 900px) {
+        grid-template-columns: repeat(2, 1fr);
+      }
+      @media (max-width: 700px) {
+        grid-template-columns: 1fr;
+      }
+    `
+    return <div className={gridStyles1}>{this.props.children}</div>
   }
 }
